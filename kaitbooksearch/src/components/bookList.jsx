3ca1d6c813d2649e08913693f57a5591c17fc1ea
System Administrator <root@Kaitlyns-MacBook-Pro.local>
2019-04-04 00:06:07 -0500

    established

---
+++ kaitbooksearch/src/components/bookList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Book from "./book";
+
+const BookList = props => {
+  return (
+    <div className="row bookList">
+        {props.books.map((book, index) => (
+          <div key={index}>
+            <Book
+              isbn={book.isbns[0].isbn10}
+              author={book.book_details[0].author}
+              bookTitle={book.book_details[0].title}
+              publisher={book.book_details[0].publisher}
+              description={book.book_details[0].description}
+              rank={book.rank}
+              weeksOnList={book.weeks_on_list}
+            />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default BookList;
